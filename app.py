2adf8a96488e1f5067a02e02d59527876a0dac6b
rosiephandang <23598@burnside.school.nz>
2026-02-16 13:02:13 +1300

    joined db to flask py

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask # render_template
+import sqlite3
+from flask import Flask, g # render_template
 
+DATABASE = 'database.db'
+
+#initialise app
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def home():
-    return "Hello World!"
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM Commercial_aircraft"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results) # for testing purposes, we will just return the results as a string
 
 if __name__ == "__main__":
     app.run(debug=True)
