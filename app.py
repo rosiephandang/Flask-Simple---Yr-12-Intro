105d5241d74d733d2089dee59573dc28adf08ec5
rosiephandang <23598@burnside.school.nz>
2026-02-20 10:14:19 +1300

    added another query (and import flask sucks for som reason)

--- app.py
+++ app.py
@@ -38,5 +38,18 @@ def home():
     results = query_db(sql)
     return str(results) # for testing purposes, we will just return the results as a string
 
+@app.route('/aircraft/<int:model_id>')
+def aircraft(model_id):
+    # return all details for a specific model of commercial aircraft
+    sql = """SELECT Commercial_aircraft.model_name,  Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name, Commercial_aircraft.top_speed_kmph, Commercial_aircraft.max_people_capacity, Commercial_aircraft.max_distance_km, Commercial_aircraft.max_takeoff_weight_kg,Commercial_aircraft.fuel_capacity_L
+FROM Commercial_aircraft
+INNER JOIN Manufacturer
+ON Commercial_aircraft.manufacture_id = Manufacturer.manufacturer_id
+INNER JOIN Fuselage
+ON Commercial_aircraft.fuselage_id = Fuselage.type_id
+WHERE Commercial_aircraft.aircraft_id = ?;"""
+    result = query_db(sql, (model_id,), one=True)
+    return str(result) # for testing purposes, we will just return the result as a string
+
 if __name__ == "__main__":
     app.run(debug=True)
