5663ecb58cbbf5e70df415ebf975b05af47caffe
rosiephandang <23598@burnside.school.nz>
2026-03-02 13:01:15 +1300

    added homepage layout & some pictures

--- app.py
+++ app.py
@@ -28,7 +28,9 @@ def query_db(query, args=(), one=False):
 def home():
     # home page - return just the model name, image URL manufacturer and fuselage for all commercial airrcraft in the database
     sql = """SELECT Commercial_aircraft.model_name,
-      Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name
+      Commercial_aircraft.image_url,
+      Manufacturer.manufacturer_name, 
+      Fuselage.type_name
         FROM Commercial_aircraft
         INNER JOIN Manufacturer
         ON Commercial_aircraft.manufacture_id = Manufacturer.manufacturer_id 
@@ -36,10 +38,10 @@ def home():
         ON Commercial_aircraft.fuselage_id = Fuselage.type_id
         ORDER BY Commercial_aircraft.model_name ASC;"""
     results = query_db(sql)
-    return render_template("layout.html")
+    return render_template("home.html", results=results)
 
-@app.route('/aircraft/<int:model_id>')
-def aircraft(model_id):
+#@app.route('/aircraft/<int:model_id>')
+#def aircraft(model_id):
     # return all details for a specific model of commercial aircraft
     sql = """SELECT Commercial_aircraft.model_name,  Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name, Commercial_aircraft.top_speed_kmph, Commercial_aircraft.max_people_capacity, Commercial_aircraft.max_distance_km, Commercial_aircraft.max_takeoff_weight_kg,Commercial_aircraft.fuel_capacity_L
 FROM Commercial_aircraft
@@ -51,8 +53,8 @@ WHERE Commercial_aircraft.aircraft_id = ?;"""
     result = query_db(sql, (model_id,), one=True)
     return str(result) # for testing purposes, we will just return the result as a string
 
-@app.route('/airbus')
-def airbus():
+#@app.route('/airbus')
+#def airbus():
     # airbus page - return just the model, manufacturer, fuselage sorted by manufacturer (Airbus)
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name
 FROM Commercial_aircraft
@@ -66,8 +68,8 @@ ORDER BY Commercial_aircraft.model_name ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/boeing')
-def boeing():
+#@app.route('/boeing')
+#def boeing():
     # boeing page - return just the model, manufacturer, fuselage sorted by manufacturer (Boeing)
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name
 FROM Commercial_aircraft
@@ -81,8 +83,8 @@ ORDER BY Commercial_aircraft.model_name ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/narrow')
-def narrow():
+#@app.route('/narrow')
+#def narrow():
     # narrow page - return just the model, manufacturer, fuselage sorted by fuselage (narrow)
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name
 FROM Commercial_aircraft
@@ -96,8 +98,8 @@ ORDER BY Commercial_aircraft.model_name ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/wide')
-def wide():
+#@app.route('/wide')
+#def wide():
     # wide page - return just the model, manufacturer, fuselage sorted by fuselage (wide)
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name
 FROM Commercial_aircraft
@@ -111,8 +113,8 @@ ORDER BY Commercial_aircraft.model_name ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/people_capacity')
-def people_capacity():
+#@app.route('/people_capacity')
+#def people_capacity():
     # people capacity page - return just the model, manufacturer, fuselage sorted by people capacity
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name,Commercial_aircraft.max_people_capacity  
 FROM Commercial_aircraft
@@ -125,8 +127,7 @@ ORDER BY Commercial_aircraft.max_people_capacity ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/max_distance')
-def max_distance():
+#def max_distance():
     # max distance page - return just the model, manufacturer, fuselage sorted by max distance
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name,Commercial_aircraft.max_distance_km  
 FROM Commercial_aircraft
@@ -139,8 +140,8 @@ ORDER BY Commercial_aircraft.max_distance_km ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/top_speed')
-def top_speed():
+#@app.route('/top_speed')
+#def top_speed():
     # top speed page - return just the model, manufacturer, fuselage sorted by top speed
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name,Commercial_aircraft.top_speed_kmph  
 FROM Commercial_aircraft
@@ -153,8 +154,8 @@ ORDER BY Commercial_aircraft.top_speed_kmph ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/max_takeoff_weight')
-def max_takeoff_weight():
+#@app.route('/max_takeoff_weight')
+#def max_takeoff_weight():
     # max takeoff weight page - return just the model, manufacturer, fuselage sorted by max takeoff weight
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name,Commercial_aircraft.max_takeoff_weight_kg  
 FROM Commercial_aircraft
@@ -167,8 +168,8 @@ ORDER BY Commercial_aircraft.max_takeoff_weight_kg ASC;"""
     return str(results) # for testing purposes, we will just return the results as a string
 
 
-@app.route('/fuel_capacity')
-def fuel_capacity():
+#@app.route('/fuel_capacity')
+#def fuel_capacity():
     # fuel capacity page - return just the model, manufacturer, fuselage sorted by fuel capacity
     sql = """SELECT Commercial_aircraft.model_name,Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name,Commercial_aircraft.fuel_capacity_L 
 FROM Commercial_aircraft
