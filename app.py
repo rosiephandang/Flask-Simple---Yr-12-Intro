c6c1ae1bde2fe2f1e838c5ebdc67687c372735b0
rosiephandang <23598@burnside.school.nz>
2026-03-10 10:20:06 +1300

    finished debugging some query/jinja/for/results loop stuff ugh

--- app.py
+++ app.py
@@ -40,18 +40,18 @@ def home():
     results = query_db(sql)
     return render_template("home.html", results=results)
 
-@app.route('/aircraft/<int:model_id>')
-def aircraft(model_id):
+@app.route('/aircraft/<int:id>')
+def aircraft(id):
     # return all details for a specific model of commercial aircraft
-    sql = """SELECT Commercial_aircraft.model_name,  Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name, Commercial_aircraft.top_speed_kmph, Commercial_aircraft.max_people_capacity, Commercial_aircraft.max_distance_km, Commercial_aircraft.max_takeoff_weight_kg,Commercial_aircraft.fuel_capacity_L
+    sql = """SELECT Commercial_aircraft.aircraft_id,Commercial_aircraft.model_name,  Commercial_aircraft.image_url,Manufacturer.manufacturer_name, Fuselage.type_name, Commercial_aircraft.top_speed_kmph, Commercial_aircraft.max_people_capacity, Commercial_aircraft.max_distance_km, Commercial_aircraft.max_takeoff_weight_kg,Commercial_aircraft.fuel_capacity_L
 FROM Commercial_aircraft
 INNER JOIN Manufacturer
 ON Commercial_aircraft.manufacture_id = Manufacturer.manufacturer_id
 INNER JOIN Fuselage
 ON Commercial_aircraft.fuselage_id = Fuselage.type_id
 WHERE Commercial_aircraft.aircraft_id = ?;"""
-    result = query_db(sql, (model_id,), one=True)
-    return render_template("plane.html", aircraft=result)
+    result = query_db(sql, (id,), one=True)
+    return render_template("plane.html", result=result)
 
 
 
